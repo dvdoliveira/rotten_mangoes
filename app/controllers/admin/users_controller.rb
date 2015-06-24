8850b64effc7d7c1fc6e2d9696eb25099f320f1f
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-06-24 01:50:34 +0000

    ÃAdd feature to create new users from admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,27 @@ class Admin::UsersController < ApplicationController
   before_filter :restrict_admin
 
   def index
-    @users = User.all
+    @users_admin = User.all
+  end
+
+  def new
+    @user_admin = User.new
+  end
+
+  def create
+    @user_admin = User.new(user_params)
+
+    if @user_admin.save
+      redirect_to admin_users_path, notice: "User was created!"
+    else
+      render :new_admin_user
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
