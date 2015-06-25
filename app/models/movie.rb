687b588f38ced6dba2ade11a3324fb8192ac7f90
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-06-25 00:51:47 +0000

    Implement search by title/director/duration + sorting + movie partial

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,6 +20,19 @@ class Movie < ActiveRecord::Base
 
   mount_uploader :movie_poster, MoviePosterUploader
 
+  scope :search_like, ->(query) { where('title LIKE :q OR director LIKE :q', q: "%#{query}%")}
+
+  scope :runtime_over, ->(time) { where('runtime_in_minutes >= ?', time) if !time.blank? }
+  scope :runtime_under, ->(time) { where('runtime_in_minutes <= ?', time) if !time.blank? }
+
+  def self.search(search)
+    if search
+      search_like(search['query']).runtime_over(search['duration_a']).runtime_under(search['duration_b'])
+    else
+      all
+    end
+  end
+
   def review_average
     if reviews.size > 0
       reviews.sum(:rating_out_of_ten)/reviews.size
