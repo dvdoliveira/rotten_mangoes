d6a7d8482ba37558f5409111467edc52eb7c1723
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-06-26 20:11:06 +0000

    Add basic movie_genre model and table references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623221430) do
+ActiveRecord::Schema.define(version: 20150626201010) do
+
+  create_table "categories", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+  end
+
+  create_table "categorization", id: false, force: true do |t|
+    t.integer "movie_id"
+    t.integer "category_id"
+  end
+
+  create_table "genres", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movie_genres", force: true do |t|
+    t.integer  "movie_id"
+    t.integer  "genre_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "movie_genres", ["genre_id"], name: "index_movie_genres_on_genre_id"
+  add_index "movie_genres", ["movie_id"], name: "index_movie_genres_on_movie_id"
 
   create_table "movies", force: true do |t|
     t.string   "title"
